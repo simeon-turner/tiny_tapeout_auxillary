172cf5602082831be29ba0d3b988226f8c0560db
smt259 <smt259@cornell.edu>
2024-10-24 19:18:03 -0400

    implemented register for input number

--- src/div_by_five.v
+++ src/div_by_five.v
@@ -14,16 +14,28 @@ module div_by_five #(
     output logic                 out_valid
 );
 
+    // assert that the bit width is divisible by 4. Bit width must be multiple of 4.
+
     // 4-bit serializer
+    logic [BIT_WIDTH-1-8:0] data_register;
+
     always_ff @( posedge clk ) begin
-    
+        if ( rst )
+            data_register <= 0;
+        else if ( data_register == 0 && carry_register == 0 ) begin
+            data_register <= in[BIT_WIDTH-1:8];
+        end else begin
+            data_register <= {4'b0, data_register[BIT_WIDTH-1-8:4]};
+        end
+
     end
 
     // Sequential addition logic
     // sum the output of the 
     // Be sure to include nuance with carry out going to the same carry in
 
-    wire [3:0] four_bit_sum;
+    logic      carry_register;  // register that stores carry bit
+    wire [3:0] four_bit_sum;    // accumulator of 4-bit wide sum of input
 
     // 4-bit div by 5 logic
     assign divisible = ~(four_bit_sum[3] ^ four_bit_sum[1]) & 
